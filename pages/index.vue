6e4f9d2f10863b742fdc0aa92527236862f66e9b
EOM Phall <123912861+phall-e@users.noreply.github.com>
2026-05-13 11:40:06 +0700

    modified home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,3 @@
 <template>
-  <div class="p-5">
-    <h1 class="text-2xl font-bold">
-      Telegram Mini App
-    </h1>
-
-    <div v-if="user">
-      <p>ID: {{ user.id }}</p>
-      <p>Name: {{ user.first_name }}</p>
-      <p>Username: {{ user.username }}</p>
-    </div>
-  </div>
+  <h1 class="text-center text-red-500">Welcome to my mini App</h1>
 </template>
-
-<script setup lang="ts">
-import { useTelegram } from '~/composables/useTelegram';
-
-const { init, user } = useTelegram()
-
-onMounted(() => {
-  init()
-})
-</script>
