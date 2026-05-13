ead419a4a23d8552ca41d8e44b2746e089e7cc78
EOM Phall <123912861+phall-e@users.noreply.github.com>
2026-05-13 12:16:23 +0700

    ft: new feature

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,18 @@
     <el-form
       label-position="top"
       label-width="auto"
-      class="w-[95%] border rouded-md p-5"
+      class="w-[95%] border rounded-md p-5"
+      @submin.prevent="handleLogin"
     >
       <el-form-item
         label="Username"
       >
-        <el-input placeholder="Username" clearable/>
+        <el-input v-model="formData.username" placeholder="Username" clearable/>
       </el-form-item>
       <el-form-item
         label="Password"
       >
-        <el-input type="password" placeholder="Password" show-password clearable/>
+        <el-input v-model="formData.password" type="password" placeholder="Password" show-password clearable/>
       </el-form-item>
       <el-form-item>
         <el-button type="success" class="!w-full">Sign In</el-button>
@@ -22,3 +23,20 @@
   </div>
 
 </template>
+
+<script setup lang="ts">
+  const formData = ref({
+    username: '',
+    password: '',
+  });
+
+
+  const handleLogin = () => {
+    ElNotification({
+      type: 'success',
+      title: 'Successfully',
+      message: 'Your logging is successfully.',
+    });
+  }
+
+</script>
